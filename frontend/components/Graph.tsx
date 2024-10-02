e00145570e2803e06d44ed094b377bf07ecebf9b
usaben <aishi2522@gmail.com>
2024-10-02 18:19:35 +0600

    usi edit

---
+++ frontend/components/Graph.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Graph: React.FC = () => {
+    return (
+        <div>
+            <h1>Graph Page</h1>
+            <p>This is the graph page.</p>
+        </div>
+    );
+};
+
+export default Graph;
---
+++ frontend/components/Statistics.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Statistics: React.FC = () => {
+    return (
+        <div>
+            <h1>Statistics Page</h1>
+            <p>This is the statistics page.</p>
+        </div>
+    );
+};
+
+export default Statistics;
